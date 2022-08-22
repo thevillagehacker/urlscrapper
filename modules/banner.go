3a989f7b99e1e39b1a95a10e2235823441602d48
Naveen J <nvnj1998@gmail.com>
2022-08-22 14:40:26 +0530

    Add files via upload

---
+++ modules/banner.go
@@ -0,0 +1,26 @@
+package banner
+
+import (
+	"fmt"
+)
+
+// banner
+const banner = `
+                  URL Scrapy
+             ------------------
+          ~ |Do Hacks to Secure| ~
+             ------------------             v1.0
+                    By
+`
+
+// Version is the current version of urlscrapy
+const Version = `v1.0`
+
+// showBanner is used to show the banner to the user
+func ShowBanner() {
+	fmt.Printf("%s\n", banner)
+	fmt.Printf("\tThe Village Hacker Security\n\n")
+
+	fmt.Printf("Use with caution. You are responsible for your actions.\n")
+	fmt.Printf("Developers assume no liability and are not responsible for any misuse or damages.\n")
+}
