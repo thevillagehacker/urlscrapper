ba567a056fafb9771dc41a9db2953f26cf657f47
Naveen J <nvnj1998@gmail.com>
2022-08-22 07:29:28 +0530

    Add files via upload

---
+++ files/banner.go
@@ -0,0 +1,23 @@
+package banner
+
+import "github.com/projectdiscovery/gologger"
+
+const banner = `
+                  URL Scrapy
+             ------------------
+          ~ |Do Hacks to Secure| ~
+             ------------------             v1.0
+                    By
+`
+
+// Version is the current version of httpx
+const Version = `v1.0`
+
+// showBanner is used to show the banner to the user
+func showBanner() {
+	gologger.Print().Msgf("%s\n", banner)
+	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
+
+	gologger.Print().Msgf("Use with caution. You are responsible for your actions.\n")
+	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
+}
