780b591dd712fb99a5c55c821e8df2c4f33474bb
Krunal Savaliya <46068669+sklnhunt@users.noreply.github.com>
2022-08-28 17:57:57 +0530

    Added file option

--- main.go
+++ main.go
@@ -1,29 +1,87 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"regexp"
+	"os"
+	"log"
+	"sync"
 
 	banner "github.com/thevillagehacker/urlscrapy/modules"
 )
+
 
 func main() {
 
 	//banner
 	banner.ShowBanner()
+	var domains []string
 
 	//flags
-	url := flag.String("u", "default value", "target url")
+	furl := flag.String("u","", "target url")
+	filePtr := flag.String("l","","list of URLs in file")
+
 	flag.Parse()
-	var target string
-	target = *url
+
+    f, err := os.Open(*filePtr)
+    if err != nil {
+        // log.Fatal(err)
+    }
+    defer func() {
+        if err = f.Close(); err != nil {
+        // log.Fatal(err)
+    }
+    }()
+    s := bufio.NewScanner(f)
+    for s.Scan() {
+        domains = append(domains, s.Text())
+    }
+    err = s.Err()
+    if err != nil {
+        // log.Fatal(err)
+    }
+  
+	var wg sync.WaitGroup
+
+	for _, u := range domains {
+	  
+	  wg.Add(1)
+	  go func(url string) {
+	  
+		defer wg.Done()
+
+		response, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer response.Body.Close()
+	
+		// Read response data in to memory
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal("Error reading HTTP body. ", err)
+		}
+		// Create a regular expression to find URLs
+		re := regexp.MustCompile("(http|https)://[a-zA-Z0-9./?=_=-]*")
+		urls := re.FindAllString(string(body), -1)
+		if urls == nil {
+			fmt.Println("No matches.")
+		} else {
+			for _, links := range urls {
+				fmt.Println(links)
+			}
+		}
+
+	  }(u)
+	}
+	wg.Wait()
 
 	// Make HTTP request
-	response, err := http.Get(target)
+	response, err := http.Get(*furl)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,3 +104,4 @@ func main() {
 		}
 	}
 }
+
